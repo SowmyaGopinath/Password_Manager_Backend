80e0017786d1ebbbd58722f4a1c1070048755468
SowmyaGopinath <sowmi.2694@gmail.com>
2024-02-12 15:44:10 +0530

    update db

--- scripts/dbconfig.js
+++ scripts/dbconfig.js
@@ -1,6 +1,6 @@
 import {MongoClient} from 'mongodb';
 
-const uri = 'mongodb://localhost:27017';
+const uri = 'mongodb+srv://password:manager@testdb.uye1dkz.mongodb.net/?retryWrites=true&w=majority';
 
 const client = new MongoClient(uri);
 
