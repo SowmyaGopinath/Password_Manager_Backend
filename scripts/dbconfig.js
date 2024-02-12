a67220cd436be4bc3bab687d03e61e951cbb26ad
G Sowmya <133192323+SowmyaGopinath@users.noreply.github.com>
2024-02-12 17:10:14 +0530

    Update dbconfig.js

--- scripts/dbconfig.js
+++ scripts/dbconfig.js
@@ -7,7 +7,7 @@ const client = new MongoClient(uri);
 async function run() {
     try {
         await client.connect();
-        const database = client.db('testdb');
+        const database = client.db('test');
         const passwordmanager = database.collection('passwordmanager');
         const query = {username: 'user1'};
         const user = await passwordmanager.findOne(query);
